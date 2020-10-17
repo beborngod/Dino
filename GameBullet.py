18b88104e7d0ed3502166881bb3346888a7e0265
beborngod <oleksiibelov@gmail.com>
2020-10-17 11:38:23 +0300

    shot with mouse

--- GameBullet.py
+++ GameBullet.py
@@ -2,17 +2,45 @@
 
 import pygame
 
+
 class GameBullet:
-    def __init__(self,display,x,y,speed=8):
+    def __init__(self, display, x, y, speed=8):
         self.display = display
         self.x = x
         self.y = y
         self.speed = speed
-        
-    def bulletMove(self, DISPLAY_WIDTH,image):
-        self.x+=self.speed
-        if self.x<=DISPLAY_WIDTH:
-            self.display.blit(image,(self.x,self.y))
+        self.speed_x = 8
+        self.speed_y = 0
+        self.dest_x = 0
+        self.dest_y = 0
+
+    def bulletMove(self, DISPLAY_WIDTH, image):
+        self.x += self.speed
+        if self.x <= DISPLAY_WIDTH:
+            self.display.blit(image, (self.x, self.y))
+            return True
+        else:
+            return False
+
+    def findPath(self, dest_x, dest_y):
+        self.dest_x = dest_x
+        self.dest_y = dest_y
+
+        delta_x = dest_x - self.x
+        count_up = delta_x//self.speed_x
+
+        if self.y >= dest_y:
+            delta_y = self.y-dest_y
+            self.speed_y = delta_y / count_up
+        else:
+            delta_y = dest_y - self.y
+            self.speed_y = -(delta_y / count_up)
+
+    def moveTo(self, image):
+        self.y -= self.speed_y
+        self.x += self.speed_x
+        if self.x <= self.dest_x:
+            self.display.blit(image, (self.x, self.y))
             return True
         else:
             return False
